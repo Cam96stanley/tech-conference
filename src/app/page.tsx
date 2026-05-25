f4ec2243884248988e04d325ee802b1741964fdd
Cameron Stanley <cam96stanley@gmail.com>
2026-05-25 15:51:09 -0400

    feat: added images

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,27 +1,8 @@
-import Button from "@/ui/primitives/Button";
-
 export default function Home() {
   return (
     <div>
-      <main className="flex gap-4">
-        <Button variant={'secondary'}>
-          Hello world
-        </Button>
-        <Button>
-          Hello world
-        </Button>
-        <Button variant={'tab'}>
-          Tab
-        </Button>
-        <Button variant={'filter'}>
-          Filter
-        </Button>
-        <Button variant={'toggle'}>
-          toggle
-        </Button>
-        <Button variant={'clear'}>
-          clear
-        </Button>
+      <main>
+        
       </main>
     </div>
   );
